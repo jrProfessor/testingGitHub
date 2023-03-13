4e98a490242ac60d4217b4da0a22043bac4893a4
professor <jr.amurriomartinez@edu.gva.es>
2023-03-13 16:44:35 +0100

    sumar

--- src/App/HelloWorld.java
+++ src/App/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!");
+        System.out.println(sumar(10, 5));
+    }
+
+    public static int sumar(int x, int y){
+        return x+y;
     }
 }
