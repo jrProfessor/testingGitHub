fb1eb0a2876d44bc33d451f88e4e625b3257d102
professor <jr.amurriomartinez@edu.gva.es>
2023-03-14 19:33:22 +0100

    multiplicar

--- src/App/HelloWorld.java
+++ src/App/HelloWorld.java
@@ -2,9 +2,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!");
         System.out.println(sumar(10, 5));
+        System.out.println(multiplicar(10, 5));
     }
 
     public static int sumar(int x, int y){
         return x+y;
+    }
+    public static int multiplicar(int x, int y){
+        return x*y;
     }
 }
