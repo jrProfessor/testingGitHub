4cb13eeeead216b9dc45dbc3e4d065090ff8d901
professor <jr.amurriomartinez@edu.gva.es>
2023-03-27 18:24:35 +0200

    Añado clase Profesor

---
+++ src/App/Model/Profesor.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Profesor {
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
